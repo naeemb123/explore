a478b6b85aca7771d091ec9dd421ab4f02204002
hamzamalik0123 <hamzamalik0123@gmail.com>
2016-11-25 19:36:08 +0000

    menu working

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,15 @@
-$(".recommendation-card").click(function () {
-    $(".recommendation-card").css("bottom","15%")
+$(document).ready(function () {
+    $(".recommendation-card").click(function () {
+        $(".recommendation-card").css("bottom","15%");
+    });
+});
+
+$(document).mouseup(function (e)
+{
+    var container = $(".recommendation-card");
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        container.css("bottom","-60%")
+    }
 });
