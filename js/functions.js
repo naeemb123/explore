2a53ed7325b64aa75a52d300c8da8034f7e052cf
hamzamalik0123 <hamzamalik0123@gmail.com>
2016-11-26 13:41:50 +0000

    minor changes

--- js/functions.js
+++ js/functions.js
@@ -19,7 +19,7 @@ $(document).mouseup(function (e)
     if (!container.is(e.target) // if the target of the click isn't the container...
         && container.has(e.target).length === 0) // ... nor a descendant of the container
     {
-        container.css("bottom","-65%");
+        container.css("bottom","-70%");
         $(".recommendation-card").html(
             "<h1 class='recommendation-places'>Recommended Places</h1>"
         );
